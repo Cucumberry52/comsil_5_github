30f0ea02838d754105949f1d4428e72f3f911387
Cucumberry52 <kjt121152@gmail.com>
2025-04-07 15:21:19 +0900

    실습1

---
+++ animal.h
@@ -0,0 +1,7 @@
+#ifndef ANIMAL_H
+#define ANIMAL_H
+void print_dog(void);
+void print_blakcow(void);
+void print_turtle(void);
+
+#endif /*ANIMAL_H*/
---
+++ blackcow.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+void print_blackcow(void){
+ printf("blackcow\n");
+}
+
+
